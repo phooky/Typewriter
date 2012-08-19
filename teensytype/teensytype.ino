909e25d70f7c540700f8c69454f26b444fdec7b6
Adam <phooky@gmail.com>
2012-08-19 15:43:27 -0400

    adding debugging input

--- teensytype/teensytype.ino
+++ teensytype/teensytype.ino
@@ -1,3 +1,6 @@
+/*
+ * This project is targeted for the Teensy 2.0
+ */
 
 /* Pin assignments
  *
@@ -60,6 +63,46 @@ void setPinDirections() {
 
 HardwareSerial Uart = HardwareSerial();
 
+typedef enum {
+  MOTOR_OFF = 0,
+  MOTOR_ON,
+  MOTOR_BRAKE
+} MotorState;
+
+void setMotor(MotorState state = MOTOR_OFF) {
+  switch (state) {
+  case MOTOR_ON:
+    digitalWrite(pin_brake, LOW);
+    delay(10);
+    digitalWrite(pin_motor, HIGH);
+    break;
+  case MOTOR_BRAKE:
+    digitalWrite(pin_motor, LOW);
+    digitalWrite(pin_brake, HIGH);
+    break;
+  case MOTOR_OFF:
+  default:
+    digitalWrite(pin_motor, LOW);
+    digitalWrite(pin_brake, LOW);
+  }
+}
+
+enum {
+  SW_1 = 0x01,
+  SW_2 = 0x02,
+  SW_3 = 0x04,
+  SW_4 = 0x08
+};
+
+uint8_t getSwitchState() {
+  uint8_t state = 0;
+  if (digitalRead(pin_switch1) == HIGH) state |= SW_1;
+  if (digitalRead(pin_switch2) == HIGH) state |= SW_2;
+  if (digitalRead(pin_switch3) == HIGH) state |= SW_3;
+  if (digitalRead(pin_switch4) == HIGH) state |= SW_4;
+  return state;
+}
+
 void setup() 
 { 
   setPinDirections();
@@ -69,6 +112,50 @@ void setup()
   Uart.println("Serial connection online."); 
 }
 
+char command_buffer[128];
+
+/*
+ * Command reference
+ * All commands are terminated by a newline ('\n') character. All responses
+ * are terminated by a newline character.
+ * Carriage return characters ('\r') are ignored.
+ * ping   - Ping the board to see if we're connected.
+ *          Returns "OK version" where "version" is an arbitrary version
+ *          string.
+ * tBR    - Type character. 
+ *          B = the number of the inductor board [0-3].
+ *          R = the number (in hex) of the inductor to use [0-11].
+ *          Returns "OK".
+ * mon    - Turn motor on.
+ *          Returns "OK".
+ * moff   - Turn motor off.
+ *          Returns "OK".
+ * mbrake - Brake motor.
+ *          Returns "OK".
+ * read   - Reads the value of the switches as a byte.
+ *          Returns a single hexidecimal character.
+ */
+
+void doCommand(char* buf) {
+  String cmd(buf);
+  if (cmd.equals("ping")) {
+    Uart.println("OK v0.01");
+  } else if (cmd.equals("mon")) {
+    setMotor(MOTOR_ON); Uart.println("OK");
+  } else if (cmd.equals("moff")) {
+    setMotor(MOTOR_OFF); Uart.println("OK");
+  } else if (cmd.equals("mbrake")) {
+    setMotor(MOTOR_BRAKE); Uart.println("OK");
+  } else if (cmd.equals("read")) {
+    Uart.println(String(getSwitchState(),HEX));
+  }
+}
+
 void loop() 
 {
+  int count = Uart.readBytesUntil('\n',command_buffer,127);
+  if (count > 0) {
+    command_buffer[count] = '\0';
+    doCommand(command_buffer);
+  }
 }
